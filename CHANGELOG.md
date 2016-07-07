6e0dec595796eb8b0412d73a80988b461223e3ab
Simon Mollweide <simon.mollweide@namics.com>
2016-07-07 08:23:44 +0200

    [new] basic setup

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## [0.1.0](https://github.com/smollweide/nms-core-ui/issues?q=milestone%3A0.7.2)
+- Initial Version
---
+++ test/index.js
@@ -0,0 +1,26 @@
+
+const utils = require('../index.js');
+//
+//import { forIn } from '../index';
+//
+//describe('forIn', () => {
+//
+//	it('loop forIn', function (done) {
+//
+//		const keys = [];
+//		const values = [];
+//
+//		forIn({
+//			a: 1,
+//			b: 2
+//		}, (key, value) => {
+//			keys.push(key);
+//			values.push(value);
+//		});
+//
+//		assert.equal(keys, ['a', 'b']);
+//		assert.equal(values, [1, 2]);
+//		done();
+//	});
+//
+//});
