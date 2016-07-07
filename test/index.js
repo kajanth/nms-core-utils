197f7c7045a78fda80bc295f68f88698975881fc
Simon Mollweide <simon.mollweide@namics.com>
2016-07-07 08:24:16 +0200

    [new] add utils.forIn

--- test/index.js
+++ test/index.js
@@ -1,26 +1,56 @@
+'use strict';
 
+const assert = require('assert');
 const utils = require('../index.js');
-//
-//import { forIn } from '../index';
-//
-//describe('forIn', () => {
-//
-//	it('loop forIn', function (done) {
-//
-//		const keys = [];
-//		const values = [];
-//
-//		forIn({
-//			a: 1,
-//			b: 2
-//		}, (key, value) => {
-//			keys.push(key);
-//			values.push(value);
-//		});
-//
-//		assert.equal(keys, ['a', 'b']);
-//		assert.equal(values, [1, 2]);
-//		done();
-//	});
-//
-//});
+
+
+describe('forIn', () => {
+
+	it('loop forIn', (done) => {
+
+		const keys = [];
+		const values = [];
+
+		utils.forIn({
+			a: 1,
+			b: 2
+		}, (key, value) => {
+			keys.push(key);
+			values.push(value);
+		});
+
+		assert.equal(keys[0], 'a');
+		assert.equal(keys[1], 'b');
+		assert.equal(values[0], 1);
+		assert.equal(values[1], 2);
+		done();
+	});
+
+	it('loop forIn - array', (done) => {
+
+		const keys = [];
+		const values = [];
+
+		utils.forIn([1, 2], (key, value) => {
+			keys.push(key);
+			values.push(value);
+		});
+
+		assert.equal(keys[0], undefined);
+		assert.equal(values[0], undefined);
+		done();
+	});
+
+	it('loop forIn - undefined', (done) => {
+
+		let counter = 0;
+
+		utils.forIn([1, 2], () => {
+			counter += 1;
+		});
+
+		assert.equal(counter, 0);
+		done();
+	});
+
+});
