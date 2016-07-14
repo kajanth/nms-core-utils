407038d3b7c0db893ce8a58c55a01ed7ed4483ba
Simon Mollweide <simon.mollweide@namics.com>
2016-07-14 07:47:39 +0200

    [lint] disable arrow-body-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-	"extends": "@namics/eslint-config/configurations/es6-node.js"
+	"extends": "@namics/eslint-config/configurations/es6-node.js",
+	"rules": {
+		"arrow-body-style": 0
+	}
 };
